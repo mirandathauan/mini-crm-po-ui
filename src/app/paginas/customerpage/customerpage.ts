5380db01fbfd4166e22d2b228e5589ed6eee8dfa
thauanm1randa <tsmiranda09@gmail.com>
2026-09-22 21:58:07 -0300

    adicionando filtro de cliente

--- src/app/paginas/customerpage/customerpage.ts
+++ src/app/paginas/customerpage/customerpage.ts
@@ -1,15 +1,19 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { PoInfoModule, PoListViewModule, PoLoadingModule, PoPageModule } from "@po-ui/ng-components";
+import { FormsModule } from '@angular/forms';
+import { PoFieldModule, PoInfoModule, PoListViewModule, PoLoadingModule, PoPageModule, PoSelectOption } from "@po-ui/ng-components";
 import { Customer } from '../../services/customer';
 
 @Component({
   selector: 'app-customerpage',
-  imports: [PoPageModule,PoListViewModule,PoInfoModule,PoLoadingModule],
+  imports: [FormsModule,PoPageModule,PoListViewModule,PoInfoModule,PoLoadingModule,PoFieldModule],
   templateUrl: './customerpage.html',
   styleUrl: './customerpage.css',
 })
 export class Customerpage implements OnInit {
+  public allCustomers: Array<Customer> = []
   public customerList: Array<Customer> = []
+  public customerOptions: Array<PoSelectOption> = []
+  public customerFiltro: number | null = null
   public isLoading = false
   #customerService = inject(Customer)
 
@@ -23,7 +27,12 @@ export class Customerpage implements OnInit {
 
     req.subscribe({
       next: (value:any) => {
+        this.allCustomers = value.items
         this.customerList = value.items
+        this.customerOptions = this.allCustomers.map((customer:any, index:number) => ({
+          label: `${customer.codigo} - ${customer.nome}`,
+          value: index
+        }))
       },
 
       error: (err:any) => {
@@ -36,5 +45,11 @@ export class Customerpage implements OnInit {
       }
     })
 
+  }
+
+  filtrarCliente():void{
+    this.customerList = this.customerFiltro !== null
+      ? [this.allCustomers[this.customerFiltro]]
+      : this.allCustomers
   }
 }
